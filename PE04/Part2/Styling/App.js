2e4ec2899275c6c45c664f68c371d212e022b474
RashmithaMothe-24 <motherashmitha@cityuniversity.edu>
2023-11-13 10:22:28 +0000

    Submission for PE04/Part2-Rashmitha Mothe

--- PE04/Part2/Styling/App.js
+++ PE04/Part2/Styling/App.js
@@ -1,26 +1,54 @@
 import React, { Component } from 'react';
-// PropTypes lets you specify what properties the ProfileCard component can accept.
 import PropTypes from 'prop-types'; 
-
-// The immutability helper function update lets you update a specific piece of the component’s state.
-// npm install immutability-helper --save
 import update from 'immutability-helper'; 
-
-/*
-  Imports the Platform utility component to programmatically select styles based on the platform.
-*/
-import { Platform, Image, StyleSheet, Text, View, TouchableHighlight } from 'react-native'; 
+import { View, Image, Text, TouchableHighlight, StyleSheet, Platform } from 'react-native'; 
 
 const userImage = require('./assets/user.png');
 
-const data = [{
+const data = [
+  {
     image: userImage,
     name: 'John Doe',
     occupation: 'Software Developer',
-    description: 'A passionate developer crating mobile applications. ',
-    showThumbnail: true
-  }
-];
+    description: 'A passionate developer creating mobile applications.',
+    showThumbnail: true,
+  },
+  {
+    image: userImage,
+    name: 'Jane Smith',
+    occupation: 'UX Designer',
+    description: 'An experienced UX designer with a focus on user-centered design.',
+    showThumbnail: true,
+  },
+  {
+    image: userImage,
+    name: 'Bob Johnson',
+    occupation: 'Data Scientist',
+    description: 'A data scientist exploring patterns and insights in big data.',
+    showThumbnail: true,
+  },
+  {
+    image: userImage,
+    name: 'Alice Brown',
+    occupation: 'Frontend Developer',
+    description: 'Passionate about creating beautiful and responsive web interfaces.',
+    showThumbnail: true,
+  },
+  {
+    image: userImage,
+    name: 'Chris Anderson',
+    occupation: 'Marketing Specialist',
+    description: 'Strategizing and executing marketing campaigns to drive business growth.',
+    showThumbnail: true,
+  },
+  {
+    image: userImage,
+    name: 'Eva Miller',
+    occupation: 'Product Manager',
+    description: 'Leading product development and innovation to meet user needs.',
+    showThumbnail: true,
+  },
+]; // Add more profile data as needed
 
 const ProfileCard = (props) => {
   const { image, name, occupation, description, onPress, showThumbnail } = props;
@@ -31,25 +59,19 @@ const ProfileCard = (props) => {
   }
 
   return (
-    <TouchableHighlight onPress={onPress}> 
-      <View style={[containerStyles]}>
+    <TouchableHighlight onPress={onPress}>
+      <View style={containerStyles}>
         <View style={styles.cardImageContainer}>
           <Image style={styles.cardImage} source={image}/>
         </View>
         <View>
-          <Text style={styles.cardName}>
-            {name}
-          </Text>
+          <Text style={styles.cardName}>{name}</Text>
         </View>
         <View style={styles.cardOccupationContainer}>
-          <Text style={styles.cardOccupation}>
-            {occupation}
-          </Text>
+          <Text style={styles.cardOccupation}>{occupation}</Text>
         </View>
         <View>
-          <Text style={styles.cardDescription}>
-            {description}
-          </Text>
+          <Text style={styles.cardDescription}>{description}</Text>
         </View>
       </View>
     </TouchableHighlight>
@@ -81,20 +103,20 @@ export default class App extends Component {
   };
   
   render() {
-    const list = this.state.data.map(function(item, index) { 
-      const { image, name, occupation, description, showThumbnail } = item;
-      return <ProfileCard key={'card-' + index}
-                   image={image}
-                   name={name}
-                   occupation={occupation}
-                   description={description}
-                   onPress={this.handleProfileCardPress.bind(this, index)}
-                   showThumbnail={showThumbnail}/>
-    }, this);
+    const profileCards = this.state.data.map((item, index) => (
+      <ProfileCard
+        key={`card-${index}`}
+        index={index}
+        onPress={() => this.handleProfileCardPress(index)}
+        {...item}
+      />
+    ));
 
     return (
       <View style={styles.container}>
-        {list} 
+        <View style={styles.row}>{profileCards.slice(0, 2)}</View>
+        <View style={styles.row}>{profileCards.slice(2, 4)}</View>
+        <View style={styles.row}>{profileCards.slice(4, 6)}</View>
       </View>
     );
   }
@@ -103,110 +125,97 @@ export default class App extends Component {
 const profileCardColor = 'dodgerblue';
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    /*
-      Adds a drop shadow to the card container based on the platform
-    */
-    cardContainer: {
-        alignItems: 'center',
-        borderColor: 'black',
-        borderWidth: 3,
-        borderStyle: 'solid',
-        borderRadius: 20,
-        backgroundColor: profileCardColor,
-        width: 300,
-        height: 400,
-        ...Platform.select({ 
-          ios: {
-            shadowColor: 'black',
-            shadowOffset: {
-              height: 10
-            },
-            shadowOpacity: 1
-          },
-          android: {
-            elevation: 15
-          }
-        })
-    },
-    /*
-      Adds a drop shadow to the circular image container
-    */
-    cardImageContainer: {
-        alignItems: 'center',
-        backgroundColor: 'white',
-        borderWidth: 3,
-        borderColor: 'black',
-        width: 120,
-        height: 120,
-        borderRadius: 60,
-        marginTop: 30,
-        paddingTop: 15,
-        ...Platform.select({ 
-          ios: {
-            shadowColor: 'black',
-            shadowOffset: {
-              height: 10,
-            },
-            shadowOpacity: 1
-          },
-          android: {
-            borderWidth: 3,
-            borderColor: 'black',
-            elevation: 15
-          }
-        })
-    },
-    cardImage: {
-        width: 80,
-        height: 80
-    },
-    /*
-        You can use the textShadowColor, textShadowOffset, and textShadowRadius properties to add a shadow to a Text element. 
-        To create a shadow, you need to specify three things:
-            The color
-            The offset
-            The radius
-
-        The offset specifies the position of the shadow relative to the component casting the shadow. 
-        The radius basically defines how blurry the shadow appears.
-    */
-    cardName: {
-        color: 'white',
-        fontWeight: 'bold',
-        fontSize: 24,
-        marginTop: 30,
-        textShadowColor: 'black',
-        textShadowOffset: {
-            height: 5,
-            width: 5
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 45, // Add marginTop to give space above the first row
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+  },
+  cardContainer: {
+    alignItems: 'center',
+    borderColor: 'black',
+    borderWidth: 3,
+    borderStyle: 'solid',
+    borderRadius: 20,
+    backgroundColor: profileCardColor,
+    width: 300,
+    height: 400,
+    ...Platform.select({ 
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: {
+          height: 10
         },
-        textShadowRadius: 3 
-    },
-    cardOccupationContainer: {
-        borderColor: 'black',
-        borderBottomWidth: 3
-    },
-    cardOccupation: {
-        fontWeight: 'bold', 
-        marginTop: 10,
-        marginBottom: 10,
-    },
-    cardDescription: {
-        fontStyle: 'italic', 
-        marginTop: 10,
-        marginRight: 40,
-        marginLeft: 40,
-        marginBottom: 10
-    },
-    /*
-        The cardThumbnail style reduces the component’s size by 80%.
-    */
-        cardThumbnail: {
-          transform: [{scale: 0.2}]
+        shadowOpacity: 1
       },
+      android: {
+        elevation: 15
+      }
+    })
+  },
+  cardImageContainer: {
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderWidth: 3,
+    borderColor: 'black',
+    width: 120,
+    height: 120,
+    borderRadius: 60,
+    marginTop: 30,
+    paddingTop: 15,
+    ...Platform.select({ 
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: {
+          height: 10,
+        },
+        shadowOpacity: 1
+      },
+      android: {
+        borderWidth: 3,
+        borderColor: 'black',
+        elevation: 15
+      }
+    })
+  },
+  cardImage: {
+    width: 80,
+    height: 80
+  },
+  cardName: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 24,
+    marginTop: 30,
+    textShadowColor: 'black',
+    textShadowOffset: {
+      height: 5,
+      width: 5
+    },
+    textShadowRadius: 3 
+  },
+  cardOccupationContainer: {
+    borderColor: 'black',
+    borderBottomWidth: 3
+  },
+  cardOccupation: {
+    fontWeight: 'bold', 
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  cardDescription: {
+    fontStyle: 'italic', 
+    marginTop: 10,
+    marginRight: 40,
+    marginLeft: 40,
+    marginBottom: 10
+  },
+  cardThumbnail: {
+    transform: [{scale: 0.2}]
+  },
 });
