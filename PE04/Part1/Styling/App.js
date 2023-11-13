bd9e5fdcefba56e64cb04fffb48b54949984dd0e
RashmithaMothe-24 <motherashmitha@cityuniversity.edu>
2023-11-13 08:28:15 +0000

    Submission for PE04/Part1

--- PE04/Part1/Styling/App.js
+++ PE04/Part1/Styling/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Image, StyleSheet, View} from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default class App extends Component<{}> {
     render() {
@@ -7,8 +7,19 @@ export default class App extends Component<{}> {
             <View style={styles.container}>
                 <View style={styles.cardContainer}>
                     <View style={styles.cardImageContainer}>
-                        <Image style={styles.cardImage}        
-                               source={require('./assets/user.png')}/>     
+                        <Image
+                            style={styles.cardImage}
+                            source={require('./assets/user.png')}
+                        />
+                    </View>
+                    <View style={styles.userInfoContainer}>
+                        <Text style={styles.userName}>John Doe</Text>
+                        <Text style={styles.userOccupation}>
+                            <Text style={styles.underlineText}>Software Developer</Text>
+                        </Text>
+                        <Text style={styles.userDescription}>
+                            A passionate developer creating amazing mobile applications.
+                        </Text>
                     </View>
                 </View>
             </View>
@@ -22,31 +33,56 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
     },
     cardContainer: {
-      alignItems: 'center',    
-      borderColor: 'black',
-      borderWidth: 3,
-      borderStyle: 'solid',
-      borderRadius: 20,
-      backgroundColor: profileCardColor,
-      width: 300,
-      height: 400
-  },
-  cardImageContainer: {
-      alignItems: 'center',      
-      backgroundColor: 'white',
-      borderWidth: 3,
-      borderColor: 'black',
-      width: 120,
-      height: 120,
-      borderRadius: 60,
-      marginTop: 30,      
-      paddingTop: 15     
-  },
-    cardImage: {      
+        alignItems: 'center',
+        borderColor: 'black',
+        borderWidth: 3,
+        borderStyle: 'solid',
+        borderRadius: 20,
+        backgroundColor: profileCardColor,
+        width: 300,
+        height: 400,
+    },
+    cardImageContainer: {
+        alignItems: 'center',
+        backgroundColor: 'white',
+        borderWidth: 3,
+        borderColor: 'black',
+        width: 120,
+        height: 120,
+        borderRadius: 60,
+        marginTop: 30,
+        paddingTop: 15,
+    },
+    cardImage: {
         width: 80,
-        height: 80
-    }
+        height: 80,
+    },
+    userInfoContainer: {
+        marginTop: 20,
+        paddingHorizontal: 20,
+        alignItems: 'center',
+    },
+    userName: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 10,
+        color: 'white',
+    },
+    userOccupation: {
+        fontSize: 18,
+        marginTop: 5,
+        color: 'white',
+    },
+    userDescription: {
+        fontSize: 14,
+        marginTop: 10,
+        textAlign: 'center',
+        color: 'white',
+    },
+    underlineText: {
+        textDecorationLine: 'underline',
+    },
 });
